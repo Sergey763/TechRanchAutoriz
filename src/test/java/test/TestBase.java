88e564d3a7a38474d91a42e3441b6db02a8ef141
Sergey Sardaev'git config --global user.name Sergey <sergeysardaev@yandex.ru>
2020-12-04 14:31:32 +0400

    relese 2

---
+++ src/test/java/test/TestBase.java
@@ -0,0 +1,26 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    WebDriver driver;
+
+    @BeforeSuite
+    public void start() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterSuite
+    public void finish() {
+        driver.quit();
+    }
+}
