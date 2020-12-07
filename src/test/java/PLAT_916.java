fb119544621b50daa9d637d12e5eabe680fb3e96
Sergey Sardaev'git config --global user.name Sergey <sergeysardaev@yandex.ru>
2020-12-07 17:14:15 +0400

    relese 8(collab method+page)

--- src/test/java/PLAT_916.java
+++ src/test/java/PLAT_916.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,11 +8,17 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import page.AutorizationPage;
+import page.DomashnyaPage;
+import page.ProfilePage;
 
 import java.util.concurrent.TimeUnit;
 
 public class PLAT_916 {
     private WebDriver driver;
+    private DomashnyaPage vhod;
+    private AutorizationPage auto;
+    private ProfilePage profil;
     private String SITE_URL_ONE = "http://platform.dev.techranch.ru/accounts/sign_in";
     private String SITE_URL_TWO = "http://platform.dev.techranch.ru/";
     private By buttonEnt = By.xpath("//div[@class=\"_1kFMl _1HmiM\"]//a");
@@ -30,6 +37,10 @@ public class PLAT_916 {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        vhod = new DomashnyaPage(driver);
+        auto = new AutorizationPage(driver);
+        profil = new ProfilePage(driver);
+
     }
 
     @AfterMethod
@@ -40,51 +51,45 @@ public class PLAT_916 {
     @Test(description = "PLAT-985 Пользователь при клике на кнопку вход в шапке происходит переход на страницу авторизации")
     public void plat985() {
         driver.get(SITE_URL_TWO);
-        driver.findElement(buttonEnt).click();
-        if (driver.findElement(emailField).isDisplayed() && driver.findElement(passwordField).isDisplayed());
+        vhod.clickVhod();
+        Assert.assertTrue(vhod.checkDisplayed());
     }
 
     @Test(description = "PLAT-928 Пользователь, на страницу авторизации успешно авторизуется вводя валидные данные. Система переходит на страницу профиля")
     public void plat928() {
         driver.get(SITE_URL_ONE);
-        driver.findElement(emailField).sendKeys(emailValid);
-        driver.findElement(passwordField).sendKeys(passwordValid);
-        driver.findElement(buttonVoyti).click();
-        driver.findElement(profilButton).click();
-        driver.findElement(exit).isDisplayed();
+        auto.autorization(emailValid, passwordValid);
+        profil.clickProfil();
+        Assert.assertTrue(profil.exitDisplayed());
     }
 
     @Test(description = "PLAT-930 Пользователь, на страницу авторизации не может авторизоваться вводя не валидные данные")
     public void plat930() {
         driver.get(SITE_URL_ONE);
-        driver.findElement(emailField).sendKeys("trololo@techranch.ru");
-        driver.findElement(passwordField).sendKeys("trololo1234");
-        driver.findElement(buttonVoyti).click();
-        driver.findElement(error).isDisplayed();
+        auto.autorization("trololo@techranch.ru", "trololo1234");
+        Assert.assertTrue(auto.strDisplayed(error));
     }
 
     @Test(description = "PLAT-931 Пользователь, на страницу авторизации не авторизуется вводя валидный email и не валидный пароль. Появляется сообщение под полем для ввода пароля: Неверный адрес email или пароль.")
     public void plat931() {
         driver.get(SITE_URL_ONE);
-        driver.findElement(emailField).sendKeys(emailValid);
-        driver.findElement(passwordField).sendKeys("trololo234");
-        driver.findElement(buttonVoyti).click();
-        driver.findElement(error).isDisplayed();
+        auto.autorization(emailValid, "trololo1234");
+        Assert.assertTrue(auto.strDisplayed(error));
     }
 
     @Test(description = "PLAT-951 Пользователь, на страницу авторизации не авторизуется оставляя поля Почта Пароль пустыми. Появляется сообщение под полем для ввода пароля: Неверный адрес email или пароль.")
     public void plat951() {
         driver.get(SITE_URL_ONE);
-        driver.findElement(buttonVoyti).click();
-        driver.findElement(error).isDisplayed();
+        auto.clickVoyti();
+        Assert.assertTrue(auto.strDisplayed(error));
     }
 
     @Test(description = "PLAT-953 Пользователь, на страницу авторизации не авторизуется введя валидную почту и оставив поле пароль пустым. Появляется сообщение под полем для ввода пароля: Неверный адрес email или пароль.")
     public void plat953() {
         driver.get(SITE_URL_ONE);
-        driver.findElement(emailField).sendKeys(emailValid);
-        driver.findElement(buttonVoyti).click();
-        driver.findElement(error).isDisplayed();
+        auto.typeEmail(emailValid);
+        auto.clickVoyti();
+        Assert.assertTrue(auto.strDisplayed(error));
     }
 }
 
