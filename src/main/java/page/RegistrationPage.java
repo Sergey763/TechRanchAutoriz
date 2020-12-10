51f9adc1598e06b505472b2564fbeb0bb0fd4c07
Sergey Sardaev'git config --global user.name Sergey <sergeysardaev@yandex.ru>
2020-12-10 17:29:24 +0400

    relese 1(createTestClass + createPageRegistration)

---
+++ src/main/java/page/RegistrationPage.java
@@ -0,0 +1,42 @@
+package page;
+
+import factory.Button;
+import factory.ErrorField;
+import factory.Field;
+import net.bytebuddy.asm.Advice;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class RegistrationPage {
+
+    public Button typeUserField;
+    public Field firstNameField;
+    public Field lastNameField;
+    public Field phoneNomberField;
+    public Field mailField;
+    public Field nameOrganizationField;
+    public Button moderationSendButton;
+    public ErrorField firstNameError;
+    public ErrorField lastNameError;
+    public ErrorField phoneError;
+    public ErrorField mailErrorExist;
+    public ErrorField mailErrorNotNull;
+
+
+    public RegistrationPage(WebDriver driver) {
+//        this.typeUserField =
+        this.moderationSendButton = new Button(driver, By.xpath("//div[@class=\"btn-holder\"]/input"));
+        this.firstNameField = new Field(driver, By.xpath("//div[@class = \"form-block form-block--first\"]/div[3]/div/input[@name = \"account[first_name]\"]"));
+        this.lastNameError = new ErrorField(driver, By.xpath("//div[@class = \"form-block form-block--first\"]/div[3]/div/div[text() = \"не может быть пустым\"]"));
+        this.lastNameField = new Field(driver, By.xpath("//div[@class = \"form-block form-block--first\"]/div[4]//div/input[@name = \"account[last_name]\"]"));
+        this.firstNameError = new ErrorField(driver,By.xpath("//div[@class = \"form-block form-block--first\"]/div[4]/div/div[text() = \"не может быть пустым\"]"));
+        this.phoneNomberField = new Field(driver, By.xpath("//div[@class = \"form-block form-block--first\"]/div[5]//div/input[@name = \"account[phone]\"]"));
+        this.phoneError = new ErrorField(driver, By.xpath("//div[@class = \"form-block form-block--first\"]/div[5]/div/div[text() = \"не может быть пустым\"]"));
+        this.mailField = new Field(driver, By.xpath("//div[@class = \"form-block form-block--first\"]/div[6]//div/input[@name = \"account[email]\"]"));
+        this.mailErrorExist = new ErrorField(driver, By.xpath("//div[@class = \"form-block form-block--first\"]/div[6]/div/div[text() = \"уже существует\"]"));
+        this.mailErrorNotNull = new ErrorField(driver, By.xpath("//div[@class = \"form-block form-block--first\"]/div[6]/div/div[text() = \"не может быть пустым\"]"));
+        this.nameOrganizationField = new Field(driver, By.xpath("//div[@class = \"form-block form-block--first\"]/div[7]//div/input[@name = \"organization[name_ru]\"]"));
+    }
+
+
+}
