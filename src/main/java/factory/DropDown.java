34ff7ec31fd0581779492d970f61b94129516b6b
Sergey Sardaev'git config --global user.name Sergey <sergeysardaev@yandex.ru>
2020-12-17 14:31:52 +0400

    relese 4(DropDown list)

--- src/main/java/factory/DropDown.java
+++ src/main/java/factory/DropDown.java
@@ -3,6 +3,9 @@ package factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class DropDown {
 
@@ -16,6 +19,12 @@ public class DropDown {
 
     public void click() {
         driver.findElement(locator).click();
+    }
+
+    public void clickList(int number) {
+        List<WebElement> checkDrops = driver.findElements(locator);
+        checkDrops.get(number).click();
+
     }
 
     public String getText() {
