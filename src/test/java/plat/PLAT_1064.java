91cff5fb424aae93b230f08491030a6cad339387
Sergey Sardaev'git config --global user.name Sergey <sergeysardaev@yandex.ru>
2020-12-25 17:01:43 +0400

    editMethods

--- src/test/java/plat/PLAT_1064.java
+++ src/test/java/plat/PLAT_1064.java
@@ -1,5 +1,6 @@
 package plat;
 
+import jcactus.PropertyLoader;
 import jcactus.generator.Generator;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -12,6 +13,7 @@ import plat.generator.PlatGenerator;
 import plat.page.ProfilePage;
 import plat.page.RegistrationPage;
 
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class PLAT_1064 {
@@ -20,14 +22,6 @@ public class PLAT_1064 {
     private RegistrationPage registrationPage;
     private ProfilePage profilePage;
     private Generator generator;
-    private String AUTORIZ_URL_TWO = "http://platform.dev.techranch.ru/accounts/sign_in";
-    private String SIGN_UP_URL = "http://platform.dev.techranch.ru/accounts/sign_up";
-    private String emailDomain = "test@";
-    private String emailNoDomain = "test";
-    private String emailExist = "test@techranch.ru";
-    private String phoneExist = "9999307766";
-    public String attributePhone = "placeholder";
-
 
     @BeforeMethod
     public void setUp() {
@@ -37,6 +31,7 @@ public class PLAT_1064 {
         registrationPage = new RegistrationPage(driver);
         profilePage = new ProfilePage(driver);
         generator = new PlatGenerator();
+
     }
 
     @AfterMethod
@@ -44,31 +39,40 @@ public class PLAT_1064 {
         driver.quit();
     }
 
-
     @Test(description = "PLAT-1105 Гость в верхнем меню сайта при нажатии на кнопку зарегистрироваться, переходит на страницу регистрации")
     public void plat1105() {
-        driver.get(AUTORIZ_URL_TWO);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.autorization"));
         registrationPage.registrationButton.click();
-        //Assert.assertTrue() ИДЕИ?
+
+        //Проверка
+        Assert.assertTrue(registrationPage.textRegistration.isDisplayed());
     }
 
     @Test(description = "PLAT-1106 У Гостя перейдя на странице регистрации в поле Тип участника по умолчанию тип Бизнес")
     public void plat1106() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
+
+        //Проверка
         Assert.assertEquals(registrationPage.typeUserDown.getText(),"Бизнес");
     }
 
     @Test(description = "PLAT-1107 Гость на странице регистрации в поле Тип участника успешно устанавливает тип СМИ")
     public void plat1107() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
+
+        //Проверка
         Assert.assertEquals(registrationPage.smiField.getText(),"СМИ");
 
     }
 
     @Test(description = "PLAT-1108 Гость при вводе валидных данных на странице регистрации успешно переходит на страницу с профилем")
     public void plat1108() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
         registrationPage.firstNameField.sendKeys(generator.getName());
         registrationPage.lastNameField.sendKeys(generator.getSurname());
@@ -76,13 +80,16 @@ public class PLAT_1064 {
         registrationPage.mailField.sendKeys(generator.getMail());
         registrationPage.nameOrganizationField.sendKeys("Testoshka");
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertTrue(profilePage.welcome.isDisplayed());
 
     }
 
     @Test(description = "PLAT-1111 Гость при вводе валидных данных на английском языке проходит регистрацию и переходит на страницу с профилем")
     public void plat1111() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
         registrationPage.firstNameField.sendKeys("English");
         registrationPage.lastNameField.sendKeys("English");
@@ -90,13 +97,16 @@ public class PLAT_1064 {
         registrationPage.mailField.sendKeys(generator.getMail());
         registrationPage.nameOrganizationField.sendKeys("English");
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertTrue(profilePage.welcome.isDisplayed());
 
     }
 
     @Test(description = "PLAT-1137 Гость при при нажатии кнопки Зарегистрироваться с валидными данными и пустым полем Имя, видит сообщение под пустым полем Не может быть пустым")
     public void plat1137() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
         registrationPage.firstNameField.sendKeys("");
         registrationPage.lastNameField.sendKeys(generator.getSurname());
@@ -104,12 +114,15 @@ public class PLAT_1064 {
         registrationPage.mailField.sendKeys(generator.getMail());
         registrationPage.nameOrganizationField.sendKeys("Testoshka");
         registrationPage.moderationSendButton.click();
+
+        //Првоерка
         Assert.assertTrue(registrationPage.firstNameError.isDisplayed());
     }
 
     @Test(description = "PLAT-1139 Гость при при нажатии кнопки Зарегистрироваться с валидными данными и пустым полем Фамилия, видит сообщение под пустым полем Не может быть пустым")
     public void plat1139() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
         registrationPage.firstNameField.sendKeys(generator.getName());
         registrationPage.lastNameField.sendKeys("");
@@ -117,13 +130,16 @@ public class PLAT_1064 {
         registrationPage.mailField.sendKeys(generator.getMail());
         registrationPage.nameOrganizationField.sendKeys("Testoshka");
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertTrue(registrationPage.lastNameError.isDisplayed());
     }
 
 
     @Test(description = "PLAT-1140 Гость при при нажатии кнопки Зарегистрироваться с валидными данными и пустым полем Телефон, видит сообщение под пустым полем Не может быть пустым")
     public void plat1140() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
         registrationPage.firstNameField.sendKeys(generator.getName());
         registrationPage.lastNameField.sendKeys(generator.getSurname());
@@ -131,12 +147,15 @@ public class PLAT_1064 {
         registrationPage.mailField.sendKeys(generator.getMail());
         registrationPage.nameOrganizationField.sendKeys("Testoshka");
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertTrue(registrationPage.phoneError.isDisplayed());
     }
 
     @Test(description = "PLAT-1141 Гость при при нажатии кнопки Зарегистрироваться с валидными данными и пустым полем Почта, видит сообщение под пустым полем Не может быть пустым")
     public void plat1141() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
         registrationPage.firstNameField.sendKeys(generator.getName());
         registrationPage.lastNameField.sendKeys(generator.getSurname());
@@ -144,12 +163,15 @@ public class PLAT_1064 {
         registrationPage.mailField.sendKeys("");
         registrationPage.nameOrganizationField.sendKeys("Testoshka");
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertTrue(registrationPage.mailErrorNotNull.isDisplayed());
     }
 
     @Test(description = "PLAT-1142 Гость при при нажатии кнопки Зарегистрироваться с валидными данными и пустым полем Наименование организации, видит сообщение под пустым полем Не может быть пустым")
     public void plat1142() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
         registrationPage.firstNameField.sendKeys(generator.getName());
         registrationPage.lastNameField.sendKeys(generator.getSurname());
@@ -157,52 +179,67 @@ public class PLAT_1064 {
         registrationPage.mailField.sendKeys(generator.getMail());
         registrationPage.nameOrganizationField.sendKeys("");
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertTrue(registrationPage.nameOrganizationError.isDisplayed());
     }
 
     @Test(description = "PLAT-1143 На странице регистрации при нажатии на поле Телефон появляется маска для ввода номера с типом +7")
     public void plat1143() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.phoneField.click();
-        Assert.assertEquals(registrationPage.phoneField.getValue(attributePhone), "+7");
+
+        //Проверка
+        Assert.assertEquals(registrationPage.phoneField.getValue(new PropertyLoader().getProperty("attributePhone")), "+7");
     }
 
     @Test(description = "PLAT-1144 На странице регистрации в поле Телефон можно вводить только цифры")
     public void plat1144() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.phoneField.sendKeys("АБВГ@&");
-        Assert.assertEquals(registrationPage.phoneField.getValue(attributePhone), "+7");
+
+        //Проверка
+        Assert.assertEquals(registrationPage.phoneField.getValue(new PropertyLoader().getProperty("attributePhone")), "+7");
     }
 
     @Test(description = "PLAT-1145 Гость на странице регистрации с типом СМИ указав валидные данные, и «Телефон» который уже зарегистрирован в системе, видит текст красными буквами «уже существует»")
     public void plat1145() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
         registrationPage.firstNameField.sendKeys(generator.getName());
         registrationPage.lastNameField.sendKeys(generator.getSurname());
-        registrationPage.phoneField.sendKeys(phoneExist);
+        registrationPage.phoneField.sendKeys(new PropertyLoader().getProperty("phoneExist"));
         registrationPage.mailField.sendKeys(generator.getMail());
         registrationPage.nameOrganizationField.sendKeys("Testoshka");
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertTrue(registrationPage.phoneExist.isDisplayed());
     }
 
     @Test(description = "PLAT-1146 Гость на странице регистрации с типом СМИ указав валидные данные, и «Почта» которая уже зарегистрирована в системе, видит текст красными буквами «уже существует»")
     public void plat1146() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
         registrationPage.firstNameField.sendKeys(generator.getName());
         registrationPage.lastNameField.sendKeys(generator.getSurname());
         registrationPage.phoneField.sendKeys(generator.getPhone());
-        registrationPage.mailField.sendKeys(emailExist);
+        registrationPage.mailField.sendKeys(new PropertyLoader().getProperty("emailExist"));
         registrationPage.nameOrganizationField.sendKeys("Testoshka");
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertTrue(registrationPage.mailErrorExist.isDisplayed());
     }
 
     @Test(description = "PLAT-1147 Гость на странице регистрации с типом СМИ заполнив все поля валидными данными, а в поле «Наименование организации» вводит уже зарегистрированную в системе такую организацию, переходит на страницу профиля")
     public void plat1147() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
         registrationPage.firstNameField.sendKeys(generator.getName());
         registrationPage.lastNameField.sendKeys(generator.getSurname());
@@ -210,23 +247,31 @@ public class PLAT_1064 {
         registrationPage.mailField.sendKeys(generator.getMail());
         registrationPage.nameOrganizationField.sendKeys("Testoshka");
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertTrue(profilePage.welcome.isDisplayed());
     }
 
     @Test(description = "PLAT-1148 Гость на странице регистрации с типом «СМИ»,заполнив поле «Почта» без домена. Появляется текст Адрес электронной почты должен содержать символ @. В адресе test отсутствует символ @.»")
     public void plat1148() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
-        registrationPage.mailField.sendKeys(emailNoDomain);
+        registrationPage.mailField.sendKeys(new PropertyLoader().getProperty("emailNoDomain"));
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertEquals(registrationPage.mailField.getTextHTML5Validation(),registrationPage.noDomain);
     }
     @Test(description = "PLAT-1149 Гость на странице регистрации с типом «СМИ», написав в поле «Почта» часть адреса с «test@». Появляется текст Введите часть адреса после символа @. Адрес test+501test@ неполный.")
     public void plat1149() {
-        driver.get(SIGN_UP_URL);
+        //Шаги
+        driver.get(new PropertyLoader().getProperty("platform.signUp"));
         registrationPage.typeUserDown.clickList(2);;
-        registrationPage.mailField.sendKeys(emailDomain);
+        registrationPage.mailField.sendKeys(new PropertyLoader().getProperty("emailDomain"));
         registrationPage.moderationSendButton.click();
+
+        //Проверка
         Assert.assertEquals(registrationPage.mailField.getTextHTML5Validation(),registrationPage.domain);
     }
 
